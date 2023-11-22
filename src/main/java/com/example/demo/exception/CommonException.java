2b49cb75393ff084789f7bc59ba24ff05cecc9d6
jiwon Moon <mon99745@dreamsecurity.com>
2023-11-22 17:30:41 +0900

    불필요한 코드 및 파일 삭제

--- src/main/java/com/example/demo/exception/CommonException.java
+++ src/main/java/com/example/demo/exception/CommonException.java
@@ -1,28 +1,9 @@
 package com.example.demo.exception;
 
 public class CommonException extends RuntimeException{
-	public CommonException(String message) {
-		this(message, null);
-	}
-
-	public CommonException(Throwable cause) {
-		this((String) null, cause);
-	}
 
 	public CommonException(String message, Throwable cause) {
 		this(null, message, cause);
-	}
-
-	public CommonException(Error error) {
-		this(error, (String) null);
-	}
-
-	public CommonException(Error error, String message) {
-		this(error, message, null);
-	}
-
-	public CommonException(Error error, Throwable cause) {
-		this(error, null, cause);
 	}
 
 	public CommonException(Error error, String message, Throwable cause) {
