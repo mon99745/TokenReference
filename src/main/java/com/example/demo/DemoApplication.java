135b19c50b966b46d03ee6b89bd75f42edde4c29
jiwon Moon <mon99745@dreamsecurity.com>
2023-12-01 17:56:08 +0900

    Refactor. 설정부 초기값 부여 및 빈생성의 에러 수정

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.config.VerifyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(VerifyProperties.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/config/VerifyProperties.java
+++ src/main/java/com/example/demo/config/VerifyProperties.java
@@ -6,13 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 검증 설정
  */
 @Data
-@Component
+@Configuration
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class VerifyProperties {
 
@@ -20,19 +20,19 @@ public class VerifyProperties {
 	 * 키 페어 경로
 	 */
 	@Value("${keyPair.path}")
-	protected String path;
+	protected String path = "C:/git-personal/demo/files/";
 
 	/**
 	 * 키 페어 생성 알고리즘
 	 */
 	@Value("${keyPair.algorithm}")
-	protected String algorithm;
+	protected String algorithm = "RSA";
 
 	/**
 	 * 키 페어 크기
 	 */
 	@Value("${keyPair.keySize}")
-	protected int keySize;
+	protected int keySize = 2048;
 
 	/**
 	 * 설정 정보
