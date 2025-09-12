ece087da49deba86d6f05943875d7892f7e3ac27
mon99745 <mon99745@gmail.com>
2025-09-12 13:44:41 +0900

    refactor. [공통] 공통 예외처리 적용

---
+++ src/main/java/com/security/jsonwebtoken/common/util/StringUtil.java
@@ -0,0 +1,49 @@
+package com.security.jsonwebtoken.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+
+/**
+ * 문자열 유틸리티
+ */
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public final class StringUtil {
+	/**
+	 * 에러 메세지 형식에 맞게 변환
+	 *
+	 * @param code    에러 코드
+	 * @param message 에러 메세지
+	 * @return 에러 메세지 예) [code]message
+	 */
+	public static String toCodeString(Object code, Object message) {
+		return String.format("[%s]%s", code, message);
+	}
+
+	/**
+	 * 문자열을 스페이스로 결합
+	 *
+	 * @param values 문자열들
+	 * @return 결합된 문자열
+	 */
+	public static String joiningBySpace(String... values) {
+		return joining(" ", values);
+	}
+
+	/**
+	 * 문자열을 구분자로 결합
+	 *
+	 * @param delimiter 구분자
+	 * @param values    문자열들
+	 * @return 결합된 문자열
+	 */
+	public static String joining(String delimiter, String... values) {
+		return Arrays.stream(values)
+				.filter(StringUtils::isNotEmpty)
+				.collect(Collectors.joining(delimiter));
+	}
+}
