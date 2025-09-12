539e993e519b857fe55c071ab28bcef59ea9890e
mon99745 <mon99745@gmail.com>
2025-09-12 13:42:08 +0900

    feature. [공통] 조건에 맞는 Bean을 자동으로 생성하도록 설정 추가

---
+++ src/main/java/com/security/jsonwebtoken/config/TokenAutoConfig.java
@@ -0,0 +1,31 @@
+package com.security.jsonwebtoken.config;
+
+import com.security.jsonwebtoken.service.KeyPairService;
+import com.security.jsonwebtoken.service.TokenService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(VerifyProperties.class)
+public class TokenAutoConfig {
+	@Bean
+	@ConditionalOnMissingBean(RsaKeyGenerator.class)
+	public RsaKeyGenerator rsaKeyGenerator(VerifyProperties verifyProperties) {
+		return new RsaKeyGenerator(verifyProperties);
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(KeyPairService.class)
+	public KeyPairService keyPairService(RsaKeyGenerator rsaKeyGenerator) {
+		return new KeyPairService(rsaKeyGenerator);
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(TokenService.class)
+	public TokenService tokenService(RsaKeyGenerator rsaKeyGenerator, VerifyProperties verifyProperties,
+									 KeyPairService keyPairService) {
+		return new TokenService(rsaKeyGenerator, verifyProperties, keyPairService);
+	}
+}
